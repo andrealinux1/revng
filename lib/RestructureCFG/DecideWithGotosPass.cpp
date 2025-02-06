593a1c6f31f798d9ccea49cb66189963e2b7baf9
Andrea Gussoni <andreagus@rev.ng>
2025-02-06 15:31:55 +0100

    DecideWithGotos: add the `Impl` class

--- lib/RestructureCFG/DecideWithGotosPass.cpp
+++ lib/RestructureCFG/DecideWithGotosPass.cpp
@@ -10,6 +10,24 @@ using namespace llvm;
 // Debug logger
 Logger<> DecideWithGotosPassLogger("decide-with-gotos");
 
+class DecideWithGotosPassImpl {
+  Function &F;
+
+public:
+  DecideWithGotosPassImpl(Function &F) : F(F) {}
+
+public:
+  bool run() {
+
+    dbg << &F << "\n";
+
+    // We keep a boolean variable to track whether the `Module` was modified
+    bool ModuleModified = true;
+
+    return ModuleModified;
+  }
+};
+
 char DecideWithGotosPass::ID = 0;
 
 // TODO: review the decision about shortening this flag for a better commandline
@@ -19,7 +37,15 @@ using Reg = llvm::RegisterPass<DecideWithGotosPass>;
 static Reg X(Flag, "Perform the DecideWithGotos pass on the ScopeGraph");
 
 bool DecideWithGotosPass::runOnFunction(llvm::Function &F) {
-  return true;
+
+  // Instantiate and call the `Impl` class
+  DecideWithGotosPassImpl DecideWithGotosImpl(F);
+  bool FunctionChanged = DecideWithGotosImpl.run();
+
+  // This pass may transform the CFG by adding some edge into `goto` edges,
+  // therefore creating some additional `goto_block`s. We propagate the
+  // information computed by the `Impl` class.
+  return FunctionChanged;
 }
 
 void DecideWithGotosPass::getAnalysisUsage(llvm::AnalysisUsage &AU) const {
