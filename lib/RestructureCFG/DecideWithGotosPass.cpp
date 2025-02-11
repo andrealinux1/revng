b0e0fab278d2433c39ad4114ebfe071864765a6b
Andrea Gussoni <andreagus@rev.ng>
2025-02-11 15:02:49 +0100

    DecideWithGotos: impl stub

--- lib/RestructureCFG/DecideWithGotosPass.cpp
+++ lib/RestructureCFG/DecideWithGotosPass.cpp
@@ -2,7 +2,12 @@
 // Copyright rev.ng Labs Srl. See LICENSE.md for details.
 //
 
+#include "llvm/ADT/PostOrderIterator.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Support/GenericDomTree.h"
+
 #include "revng/RestructureCFG/DecideWithGotosPass.h"
+#include "revng/RestructureCFG/ScopeGraphGraphTraits.h"
 #include "revng/Support/Debug.h"
 
 using namespace llvm;
@@ -19,10 +24,43 @@ public:
 public:
   bool run() {
 
-    dbg << &F << "\n";
-
-    // We keep a boolean variable to track whether the `Module` was modified
+    // We keep a boolean variable to track whether the `Module` was modified.
+    // TODO: assign the initializer to `false`, and reassign it when the first
+    //       change is made.
     bool ModuleModified = true;
+
+    Function *PF = &F;
+    Scope<Function *> ScopeGraph(PF);
+
+    dbg << "Reverse post order:\n";
+    for (BasicBlock *RPONode : llvm::ReversePostOrderTraversal(ScopeGraph)) {
+      dbg << RPONode->getName().str() << "\n";
+    }
+
+    // We iterate over the conditional nodes in the `ScopeGraph` in post order
+    // TODO: verify that processing the conditional nodes in post order is the
+    //       legit thing to do
+    for (BasicBlock *PONode : llvm::post_order(ScopeGraph)) {
+
+      // TODO: Do we need to take into account the edges on the `ScopeGraph` for
+      //       electing the conditional nodes? `goto` edges should not be taken
+      //       into account, but what about `scope-closer` edges? Do they count
+      //       toward making a node a conditional node?
+      auto Successors = llvm::children<Scope<BasicBlock *>>(PONode);
+      // size_t NumSuccessors = std::ranges::size(Successors);
+      // size_t NumSuccessors = std::distance(Successors);
+      size_t NumSuccessors = std::distance(Successors.begin(),
+                                           Successors.end());
+      // We skip all the nodes which are not conditional
+      if (NumSuccessors <= 1) {
+        continue;
+      }
+
+      // Find the postdominator of `PONode` on the `ScopeGraph`
+      llvm::PostDomTreeOnView<llvm::BasicBlock, Scope> PDT;
+      PDT.recalculate(*PF);
+      PDT.print(llvm::dbgs());
+    }
 
     return ModuleModified;
   }
